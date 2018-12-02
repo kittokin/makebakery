0444e8110651f96c0b53cf2016e8a20fdbd80514
Michael F. Lamb <mike@datagrok.org>
2018-12-02 09:52:07 -0800

    rename raw to run after paths_are_dirs

--- modules/___raw/module_out.mk
+++ modules/___raw/module_out.mk
@@ -1,6 +1,5 @@
 # "raw" files will be saved as-is even if their next higher
-# extension is .html. This must come after any plugins that
-# remove .html extensions.
+# extension is .html.
 targets := $(targets:.raw=)
 $(DST)/%: $(DST)/%.raw
 	cp -p $< $@
