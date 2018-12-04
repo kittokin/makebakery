7e3f8f433b538b6d3b2c317fa9e4b0e368098589
Michael F. Lamb <mike@datagrok.org>
2018-12-04 15:13:24 -0800

    improve pandoc/m4 optional integration
    - give pandoc passthru template a more explicit name
    - new make variables to control pandoc: PANDOC_TEMPLATE, PANDOC_HEADER,
      PANDOC_BEFORE_BODY, PANDOC_AFTER_BODY, PANDOC_STYLESHEET,
      PANDOC_TEMPLATE

--- modules/pandoc/module.mk
+++ modules/pandoc/module.mk
@@ -1,16 +1,48 @@
-# email obfuscation=none so that email addresses specified in author field
+# - email obfuscation=none so that email addresses specified in author field
 # don't get replaced by huge script tags.
-default_pandoc_template := $(dir $(lastword $(MAKEFILE_LIST)))/template.html
-PANDOC_TEMPLATE ?= $(default_pandoc_template)
+# - --template implies --standalone
+
+PANDOC_ARGS = \
+	-f markdown+pipe_tables+header_attributes+smart \
+	-t html5 \
+  --base-header-level=2 \
+  --strip-comments \
+	--email-obfuscation=none \
+	  --standalone
+
+ifdef PANDOC_TEMPLATE
+PANDOC_ARGS += --template=$(PANDOC_TEMPLATE)
+endif
+ifdef PANDOC_HEADER
+PANDOC_ARGS += --include-in-header=$(PANDOC_HEADER)
+endif
+ifdef PANDOC_BEFORE_BODY
+PANDOC_ARGS += --include-before-body=$(PANDOC_BEFORE_BODY)
+endif
+ifdef PANDOC_AFTER_BODY
+PANDOC_ARGS += --include-after-body=$(PANDOC_AFTER_BODY)
+endif
+ifdef PANDOC_STYLESHEET
+PANDOC_ARGS += --css $(PANDOC_STYLESHEET)
+endif
+
 ifdef m4_templates
+
+PANDOC_TEMPLATE ?= $(dir $(lastword $(MAKEFILE_LIST)))/m4_passthrough.template
 targets := $(targets:.html.m4.md=.html.m4)
 targets := $(targets:.md=.html.m4)
-$(DST)/%.html.m4: $(DST)/%.md $(PANDOC_TEMPLATE)
-	pandoc -f markdown+smart -t html5 --base-header-level 2 --email-obfuscation=none --template=$(PANDOC_TEMPLATE) -o $@ $<
-$(DST)/%.html.m4: $(DST)/%.html.m4.md $(PANDOC_TEMPLATE)
-	pandoc -f markdown+smart -t html5 --base-header-level 2 --email-obfuscation=none --template=$(PANDOC_TEMPLATE) -o $@ $<
+
+$(DST)/%.html.m4: $(DST)/%.md $(PANDOC_TEMPLATE) $(PANDOC_HEADER) $(PANDOC_BEFORE_BODY) $(PANDOC_AFTER_BODY)
+	pandoc $(PANDOC_ARGS) $< > $@
+
+$(DST)/%.html.m4: $(DST)/%.html.m4.md $(PANDOC_TEMPLATE) $(PANDOC_HEADER) $(PANDOC_BEFORE_BODY) $(PANDOC_AFTER_BODY)
+	pandoc $(PANDOC_ARGS) $< > $@
+
 else
+
 targets := $(targets:.md=.html)
-$(DST)/%.html: $(DST)/%.md $(PANDOC_TEMPLATE)
-	pandoc -f markdown+smart -t html5 --base-header-level 2 --email-obfuscation=none --template=$(PANDOC_TEMPLATE) -o $@ $<
+
+$(DST)/%.html: $(DST)/%.md $(PANDOC_TEMPLATE) $(PANDOC_HEADER) $(PANDOC_BEFORE_BODY) $(PANDOC_AFTER_BODY)
+	pandoc $(PANDOC_ARGS) $< > $@
+
 endif
