dfe6c451f7c3ad208e4277bf1cf428830a3f7cc7
Michael F. Lamb <mike@datagrok.org>
2021-05-03 04:56:13 -0700

    move modules docs into documentation site

--- modules/extensionless_html/module.mk
+++ modules/extensionless_html/module.mk
@@ -8,6 +8,11 @@
 # See COPYING for details.
 #
 
+# DEPRECATED
+#
+# This module is deprecated. paths_are_dirs works better and easier and I don't
+# feel like supporting this one.
+
 # causes all .html files to be saved with no extension. For this to work right
 # you should configure your webserver to set the Content-type header for these
 # files to text/html. You might do this by setting the default mime-type to
@@ -18,6 +23,13 @@
 # filename extensions, if your webserver can figure out a resource's file type
 # through other means.) Apache "multiviews" or the "paths_are_dirs.mk" plugins
 # are other ways to do the same.
+#
+# If you have multiple levels of source files, then /page will conflict with
+# /page/child; /page can't be both a file and a directory at once. possible solutions:
+# - You could modify this module to cause files to have some kind of prefix, or
+# - You could modify this module to cause directories to have some kind of suffix, or
+# - You could use paths_are_dirs
+
 targets := $(targets:.html=)
 $(DST)/%: $(DST)/%.html
 	cp -p $< $@
