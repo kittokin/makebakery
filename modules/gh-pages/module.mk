756ee633d37b56c3d17e08531e0356b648eeee79
Michael F. Lamb <mike@datagrok.org>
2021-05-03 19:50:06 -0700

    gh-pages: calling $(MAKE) doesn't clean out intermediates

--- modules/gh-pages/module.mk
+++ modules/gh-pages/module.mk
@@ -10,7 +10,16 @@
 
 .PHONY: gh-pages
 
-gh-pages: all
+# NOTE: The GNU Make manual says,
+#
+# "Recursive make commands should always use the variable MAKE, not the
+# explicit command name ‘make’" (5.7.1)
+#
+# Unfortunately, there seems to be a feature where the toplevel Makefile will
+# not clean up its intermediate files until sub-makes have completed execution.
+# That's undesired behavior in this case.
+
+gh-pages:
 	# Ensuring there are no uncommitted changes in the current branch:
 	git status -s
 	[ -z "$$(git status -s)" ] || ( echo "Error: commit or stash local changes first." && false )
@@ -20,7 +29,10 @@ gh-pages: all
 	[ "$$(git symbolic-ref HEAD)" != "refs/heads/gh-pages" ]
 	# Ensuring $(DST) is a clone of this repo and gh-pages branch is checked out there:
 	[ "$$(git -C $(DST) symbolic-ref HEAD)" = "refs/heads/gh-pages" ]
-	$(MAKE) all
-	git -C $(DST) add .
-	git -C $(DST) commit -a -m "Result of 'make gh-pages' against commit $$(git rev-parse --short HEAD)"
-	git -C $(DST) push
+	make # Why not $(MAKE)? See NOTE above.
+	# This pushes the gh-pages branch that we updated in DST back into our own
+	# local repo, not our upstream. It's still up to us to push to GitHub after
+	# this.
+	git -C $(DST) commit -a -m "Result of 'make gh-pages' against commit $$(git rev-parse --short HEAD)" \
+		&& git -C $(DST) push \
+		|| echo "Not updating gh-pages branch."
